d590df77173bb4eb1ee79835936653bff1bd8548
s28491 <s28491@pjwstk.edu.pl>
2025-03-28 22:24:27 +0100

    Dodane klasy Kontener i OverfillException

---
+++ CW-2-s28491/Kontener.cs
@@ -0,0 +1,37 @@
+﻿namespace CW_2_s28491;
+
+public abstract class Kontener
+{
+    private static int _int = 1;
+    public string NumerSeryjny { get; }
+    public double WagaWlasna { get; }
+    public double MaksymalnaLadownosc { get; }
+    public double MasaLadunku { get; protected set; }
+
+    protected Kontener(string typ, double wagaWlasna, double maksLadownosc)
+    {
+        NumerSeryjny = $"KON-{typ}-{_int++}";
+        WagaWlasna = wagaWlasna;
+        MaksymalnaLadownosc = maksLadownosc;
+        MasaLadunku = 0;
+    }
+
+    public virtual void Zaladuj(double masa)
+    {
+        if (MasaLadunku + masa > MaksymalnaLadownosc)
+        {
+            throw new OverfillException($"Przekroczono maksymalną ładowność kontenera {NumerSeryjny}");
+        }
+        MasaLadunku += masa;
+    }
+
+    public virtual void Oproznij()
+    {
+        MasaLadunku = 0;
+    }
+
+    public override string ToString()
+    {
+        return $"{NumerSeryjny} | Masa ładunku: {MasaLadunku}kg";
+    }
+}
---
+++ CW-2-s28491/OverfillException.cs
@@ -0,0 +1,6 @@
+﻿namespace CW_2_s28491;
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) { }
+}
