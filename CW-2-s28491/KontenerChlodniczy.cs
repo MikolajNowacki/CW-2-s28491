6ce2e383ef7459981fa9feebe31752a9d5e6b744
s28491 <s28491@pjwstk.edu.pl>
2025-03-29 00:48:59 +0100

    Dodanie sprawdzania czy kontenerowiec spełnia maksymalną temerature produktu

--- CW-2-s28491/KontenerChlodniczy.cs
+++ CW-2-s28491/KontenerChlodniczy.cs
@@ -8,14 +8,17 @@ public class KontenerChlodniczy : Kontener
     public KontenerChlodniczy(double wysokosc, double glebokosc, double wagaWlasna, double maksLadownosc, string produkt, double temperatura)
         : base("C", wysokosc, glebokosc, wagaWlasna, maksLadownosc)
     {
+        if (!ProduktChlodniczy.CzyProduktZdefiniowany(produkt))
+            throw new Exception($"Produkt {produkt} nie jest wspierany.");
+
+        var (minTemp, maxTemp) = ProduktChlodniczy.PobierzZakresTemperatury(produkt);
+
+        if (temperatura < minTemp || temperatura > maxTemp)     // dodałem też sprawdzanie maksymalnej temperatury w kontenerowcu (nie ma tego w wymaganiach ale ma to dla mnie większy sens)
+        {
+            throw new Exception($"Temperatura {temperatura}°C jest poza dopuszczalnym zakresem dla produktu {produkt}. Dozwolony zakres: {minTemp}°C - {maxTemp}°C.");
+        }
+
         RodzajProduktu = produkt;
         Temperatura = temperatura;
-    }
-
-    public override void Zaladuj(double masa)
-    {
-        if (masa > MaksymalnaLadownosc)
-            throw new OverfillException("Zbyt duzo ladunku do kontenera chlodniczego");
-        base.Zaladuj(masa);
     }
 }
