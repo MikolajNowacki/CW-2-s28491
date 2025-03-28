b643067de7d541b71e654b699ee5513d9fb6ecbb
s28491 <s28491@pjwstk.edu.pl>
2025-03-29 00:45:30 +0100

    Dodana maksymalna temperatura dla produktu

--- CW-2-s28491/ProduktChlodniczy.cs
+++ CW-2-s28491/ProduktChlodniczy.cs
@@ -2,30 +2,30 @@
 
 public static class ProduktChlodniczy
 {
-    private static readonly Dictionary<string, double> _minimalneTemperatury = new(StringComparer.OrdinalIgnoreCase)
+    private static readonly Dictionary<string, (double min, double max)> _zakresy = new(StringComparer.OrdinalIgnoreCase) // dodałem też sprawdzanie maksymalnej temperatury w kontenerowcu (nie ma tego w wymaganiach ale ma dla mnie większy sens)
     {
-        { "Bananas", 13.3 },
-        { "Chocolate", 18 },
-        { "Fish", 2 },
-        { "Meat", -15 },
-        { "Ice cream", -18 },
-        { "Frozen pizza", -30 },
-        { "Cheese", 7.2 },
-        { "Sausages", 5 },
-        { "Butter", 20.5 },
-        { "Eggs", 19 }
+        { "Bananas", (13.3, 14) },
+        { "Chocolate", (18, 21) },
+        { "Fish", (2, 4) },
+        { "Meat", (-15, -10) },
+        { "Ice cream", (-18, -16) },
+        { "Frozen pizza", (-30, -28) },
+        { "Cheese", (7.2, 10) },
+        { "Sausages", (5, 7) },
+        { "Butter", (20.5, 22) },
+        { "Eggs", (19, 22) }
     };
 
     public static bool CzyProduktZdefiniowany(string nazwa)
     {
-        return _minimalneTemperatury.ContainsKey(nazwa);
+        return _zakresy.ContainsKey(nazwa);
     }
 
-    public static double PobierzMinimalnaTemperature(string nazwa)
+    public static (double min, double max) PobierzZakresTemperatury(string nazwa)
     {
-        if (_minimalneTemperatury.TryGetValue(nazwa, out double temperatura))
+        if (_zakresy.TryGetValue(nazwa, out var zakres))
         {
-            return temperatura;
+            return zakres;
         }
 
         throw new ArgumentException($"Nieznany produkt: {nazwa}");
